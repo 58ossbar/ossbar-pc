97229da1cf199c9f10239b7dced3af53a2339f96
hujun <hujun@creatorblue.com>
2022-09-15 16:41:45 +0800

    修改本地接口地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"development"',
-  //baseUrl: '"https://edu.creatorblue.com/console/"',
-  baseUrl: '"http://localhost:9999/"',
-  doMainUrl: '"https://frp.creatorblue.com"'
+  baseUrl: '"http://localhost:19999/"',
+  doMainUrl: '"http://localhost"'
 }
